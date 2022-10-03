dba4591348f9123519e35e26dff2bbadc13a0391
catpea <corporate.miou@gmail.com>
2022-10-03 16:54:45 -0400

    New Release

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import {analysis, log, exists} from './api.js';
 import path from 'path';
+import {kebabCase} from 'lodash-es';
 import morgan from 'koa-morgan';
 import { createReadStream } from 'fs';
 const logger = morgan('combined');
@@ -9,13 +10,16 @@ export default async function main(conf){
   const app = new Koa(conf.koa);
   app.context.conf = conf;
   app.context.hosts = Object.assign(...conf.virtual.filter(entry=>entry.default).map(entry=>({default: entry})), ...conf.virtual.map(entry=>({[entry.name]: entry})) );
-  app.use(morgan('combined'))
+  Object.entries(conf.morgan.tokens).map(([key, val])=>morgan.token(kebabCase(key), (req, res) => req.headers[val]));
+  app.use(morgan('combined', { skip: (req, res) => { return !conf.morgan.enabled } }))
   app.use(virtualServer);
   return app;
 }
 
-
 async function virtualServer(ctx) {
+
+  if (ctx.conf.debug && ctx.path=='/header') return ctx.body = ctx.request.header;
+  if (ctx.conf.debug && ctx.path=='/ip') return ctx.body = {ip: ctx.request.ip};
 
   // request analysis
   const host = ctx.hosts[ctx.hostname]||ctx.hosts.default;
--- test.js
+++ test.js
@@ -1,33 +1,64 @@
+import { strict as assert } from 'assert';
 import sf from './index.js';
-const conf = {koa:{}, virtual:[ { name: 'stellar-fox.tld', root:'fake-www', default: true}, ], };
+const conf = {
+  
+  koa:{
+    proxy: true,
+    proxyIpHeader: 'x-forwarded-for',
+  },
+
+  morgan:{
+    enabled: true,
+    tokens: {
+      remoteAddr: 'x-forwarded-for',
+    },
+  },
+
+  virtual:[
+    { 
+      name: 'stellar-fox.tld',
+      root:'fake-www',
+      default: true
+    }
+  ]
+  
+};
 import supertest from 'supertest';
 const server = (await sf(conf)).listen(process.env.PORT);
 const request = supertest.agent(server);
 
 describe('Debug Virtual Host', () => {
-  before(() => conf.debug = true);
+  before(() => {
+    conf.debug = true;
+    conf.morgan.enabled = false;
+  });
   after(() => server.close());
   describe('default vhost behavior', () => {
     describe('when requests', () => {
-      it('should 200 for index.html', done => { request.get('/').set('Host', '127.0.0.1').expect(200, done) });
-      it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
-      it('should 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('Host', '127.0.0.1').expect(404, done) });
-      it('should 403 for folder without index.html', done => { request.get('/empty-directory').set('Host', '127.0.0.1').expect(403, done) });
-      it('should redirect for dir without slash', done => { request.get('/abc').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
+      it('koa\'s ctx.request.ip should return ip from x-forwarded-for', done => { request.get('/ip') .set('x-forwarded-for','69.69.69.69') .set('Host', '127.0.0.1') .expect(res => assert.equal(res.body.ip, '69.69.69.69')) .expect(200, done) });
+      it('should 200 for index.html', done => { request.get('/').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect(200, done) });
+      it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('x-forwarded-for','69.69.69.69').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
+      it('should 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect(404, done) });
+      it('should 403 for folder without index.html', done => { request.get('/empty-directory').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect(403, done) });
+      it('should redirect for dir without slash', done => { request.get('/abc').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
     });
   });
 });
 
 describe('Live Virtual Host Behavior', () => {
-  before(() => conf.debug = false);
+  before(() => {
+    conf.debug = false;
+    conf.morgan.enabled = false;
+  });
   after(() => server.close());
   describe('default vhost behavior', () => {
     describe('when requests', () => {
-      it('should 200 for index.html', done => { request.get('/').set('Host', '127.0.0.1').expect(200, done) });
-      it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
-      it('should not 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
-      it('should not 403 for folder without index.html', done => { request.get('/empty-directory').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
-      it('should redirect for dir without slash', done => { request.get('/abc').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
+      it('should 200 for index.html', done => { request.get('/').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect(200, done) });
+      it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('x-forwarded-for','69.69.69.69').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
+      it('should not 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
+      it('should not 403 for folder without index.html', done => { request.get('/empty-directory').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
+      it('should redirect for dir without slash', done => { request.get('/abc').set('x-forwarded-for','69.69.69.69').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
+      
     });
   });
 });
