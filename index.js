21fee15bf7f0def60bbe65d3df202dd510b54598
catpea <corporate.miou@gmail.com>
2022-10-03 09:11:19 -0400

    New Release

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ import {analysis, log, exists} from './api.js';
 import path from 'path';
 import { createReadStream } from 'fs';
 
-export default async function main(config){
+export default async function main(conf){
   const app = new Koa();
-  app.context.hosts = Object.assign(...config.virtual.filter(entry=>entry.default).map(entry=>({default: entry})), ...config.virtual.map(entry=>({[entry.name]: entry})) );
+  app.context.conf = conf;
+  app.context.hosts = Object.assign(...conf.virtual.filter(entry=>entry.default).map(entry=>({default: entry})), ...conf.virtual.map(entry=>({[entry.name]: entry})) );
   app.use(virtualServer);
   return app;
 }
 
 async function virtualServer(ctx) {
+
+  // request analysis
   const host = ctx.hosts[ctx.hostname]||ctx.hosts.default;
   const request = await analysis(host.root, ctx.path); log(request);
-  if(request.error) ctx.throw(request.error);
-  if(request.redirect) ctx.redirect(request.redirect);
+
+  // smart redirects
+  if (ctx.conf.debug && request.error) ctx.throw(request.error);
+  if (!ctx.conf.debug && request.error) return ctx.redirect('/');
+  if (request.redirect) ctx.redirect(request.redirect);
+  
+  // default response
   ctx.type = path.extname(request.location);
-  ctx.body = createReadStream(request.location);
+  const dataStream = createReadStream(request.location);
+  dataStream.on('error',(err)=>log(err));
+  ctx.body = dataStream;
+
 }
--- test.js
+++ test.js
@@ -4,15 +4,32 @@ import supertest from 'supertest';
 const server = (await sf(conf)).listen(process.env.PORT);
 const request = supertest.agent(server);
 
-describe('Virtual Host', () => {
+describe('Debug Virtual Host', () => {
+  before(() => conf.debug = true);
   after(() => server.close());
-  describe('default vhost behavious', () => {
-    describe('when requestes', () => {
+  describe('default vhost behavior', () => {
+    describe('when requests', () => {
       it('should 200 for index.html', done => { request.get('/').set('Host', '127.0.0.1').expect(200, done) });
       it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
       it('should 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('Host', '127.0.0.1').expect(404, done) });
       it('should 403 for folder without index.html', done => { request.get('/empty').set('Host', '127.0.0.1').expect(403, done) });
-      it('should redirect for dir withut slassh', done => { request.get('/abc').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
-     });
+      it('should redirect for dir without slash', done => { request.get('/abc').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
+    });
   });
 });
+
+describe('Live Virtual Host Behavior', () => {
+  before(() => conf.debug = false);
+  after(() => server.close());
+  describe('default vhost behavior', () => {
+    describe('when requests', () => {
+      it('should 200 for index.html', done => { request.get('/').set('Host', '127.0.0.1').expect(200, done) });
+      it('should 200 for image/jpeg portfolio.jpg', done => { request.get('/abc/little.jpg').set('Host', 'catpea.com').expect('Content-Type', "image/jpeg").expect(200, done) });
+      it('should not 404 for gibberish', done => { request.get('/b98e89b2-47fd-4f54-96b1-13eeff7a2cf5').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
+      it('should not 403 for folder without index.html', done => { request.get('/empty').set('Host', '127.0.0.1').expect('Location', "/").expect(302, done) });
+      it('should redirect for dir without slash', done => { request.get('/abc').set('Host', '127.0.0.1').expect('Location', "/abc/").expect(302, done) });
+    });
+  });
+});
+
+ 
