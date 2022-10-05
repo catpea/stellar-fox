363e90e32b9ec16fe9713a7934f1e5a1fce9e5cc
catpea <corporate.miou@gmail.com>
2022-10-05 13:19:00 -0400

    New Release

--- foo.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-const x = path.resolve('../../../../asdas.sss/../secret/foo/foo.xt/foo/').split(path.sep).some(frag=>frag.startsWith('.'))
-// .startsWith('.')
-
-console.log(x);
